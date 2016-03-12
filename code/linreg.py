778690191b8432ed5ba4fbf466b0bc1ee68edafe
zkan <kan@prontomarketing.com>
2016-03-12 11:15:11 +0700

    Implement gradient descent to learn theta

--- code/linreg.py
+++ code/linreg.py
@@ -23,3 +23,26 @@ class LinearRegression:
         J = (1.0 / 2.0) * self.dot_product(errors, errors)
 
         return J
+
+    def run_gradient_descent(self, X, y, theta, number_of_iterations):
+        alpha = 0.01
+
+        for each_iteration in xrange(number_of_iterations):
+            predictions = []
+            for each_example in X:
+                predictions.append(self.dot_product(each_example, theta))
+
+            for t_index, each_theta in enumerate(theta):
+                diff = []
+                X_col = []
+                for p_index, each_prediction in enumerate(predictions):
+                    diff.append(each_prediction - y[p_index])
+                    X_col.append(X[t_index][p_index])
+
+                error = self.dot_product(diff, X_col)
+                result = each_theta - (alpha * error)
+                theta[t_index] = result
+
+            J = self.compute_cost(X, y, theta)
+
+        return (J, theta)
--- code/test_linreg.py
+++ code/test_linreg.py
@@ -47,6 +47,34 @@ class LinearRegressionTest(unittest.TestCase):
         expected = 2.105
         self.assertEquals(result, expected)
 
+    def test_run_gradient_descent_with_one_iteration_should_change_theta(self):
+        X = [[1, 2], [3, 4]]
+        y = [2, 2.5]
+        theta = [0, 0]
+        number_of_iterations = 1
+
+        result = self.linreg.run_gradient_descent(X, y, theta, number_of_iterations)
+
+        expected = (
+            2.6572999999999993,
+            [0.07, 0.16]
+        )
+        self.assertEquals(result, expected)
+
+    def test_run_gradient_descent_with_ten_iteration_should_change_theta(self):
+        X = [[1, 2], [3, 4]]
+        y = [2, 2.5]
+        theta = [0, 0]
+        number_of_iterations = 10
+
+        result = self.linreg.run_gradient_descent(X, y, theta, number_of_iterations)
+
+        expected = (
+            0.2953245352067722,
+            [0.21850056185143937, 0.5469413946894416]
+        )
+        self.assertEquals(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
