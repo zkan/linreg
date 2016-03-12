2542f77ddec1543ef8b0b0380d65842d9856114d
zkan <kan@prontomarketing.com>
2016-03-12 10:32:26 +0700

    Return -1 if vectors have different length

--- code/linreg.py
+++ code/linreg.py
@@ -1,7 +1,9 @@
 class LinearRegression:
     def dot_product(self, a, b):
-        result = 0
+        if len(a) != len(b):
+            return -1
 
+        result = 0
         for each in xrange(len(a)):
             result += a[each] * b[each]
 
--- code/test_linreg.py
+++ code/test_linreg.py
@@ -4,14 +4,27 @@ from linreg import LinearRegression
 
 
 class LinearRegressionTest(unittest.TestCase):
-    def test_dot_product_for_two_vectors_should_get_dot_product_result(self):
+    def setUp(self):
+        self.linreg = LinearRegression()
+
+    def test_dot_product_of_two_vectors_should_get_dot_product_result(self):
         a = [1, 2, 3]
         b = [4, 5, 6]
 
-        linreg = LinearRegression()
-        result = linreg.dot_product(a, b)
+        result = self.linreg.dot_product(a, b)
 
         expected = 32
+        self.assertEquals(result, expected)
+
+    def test_dot_product_should_get_negative_if_vectors_have_different_length(
+        self
+    ):
+        a = [1, 2, 3]
+        b = [4, 5, 6, 7]
+
+        result = self.linreg.dot_product(a, b)
+
+        expected = -1
         self.assertEquals(result, expected)
 
 
